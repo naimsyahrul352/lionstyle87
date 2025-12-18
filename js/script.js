f66619b6d9d91625e082e4b3465067b168c4b118
syahrulNaim2021430 <127114355+syahrulNaim2021430@users.noreply.github.com>
2025-12-18 09:39:18 +0700

    website untuk brand local lionstyle87

--- js/script.js
+++ js/script.js
@@ -5,6 +5,50 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+//menu scroll horizontal
+const productList = document.querySelector(".menu .ro .row");
+
+function scrollProdukKiri() {
+  productList.scrollBy({
+    left: -300,
+    behavior: "smooth",
+  });
+}
+
+function scrollProdukKanan() {
+  productList.scrollBy({
+    left: 300,
+    behavior: "smooth",
+  });
+}
+
+let isDown = false;
+let startX;
+let scrollLeftPos;
+
+productList.addEventListener("mousedown", (e) => {
+  isDown = true;
+  productList.classList.add("active");
+  startX = e.pageX - productList.offsetLeft;
+  scrollLeftPos = productList.scrollLeft;
+});
+
+productList.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+productList.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+productList.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - productList.offsetLeft;
+  const walk = (x - startX) * 2; // kecepatan drag
+  productList.scrollLeft = scrollLeftPos - walk;
+});
+
 // klik diuar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
